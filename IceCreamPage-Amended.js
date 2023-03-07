fca3b73c57c3cb13dc9b627f8738e0737f3c103e
Gregory Leclercq <125293179+Gregoratsu@users.noreply.github.com>
2023-03-07 14:52:01 +0100

    Add files via upload

--- IceCreamPage-Amended.js
+++ IceCreamPage-Amended.js
@@ -3,40 +3,50 @@ $(document).ready(function () {
     $('#vanilla').click(function () {
         if (this.checked) {
             $('#vanillaquantity').show();
+            $('#vanilla-img').show();
         } else {
             $('#vanillaquantity').hide();
+            $('#vanilla-img').hide();
         }
     })
 
     $('#chocolate').click(function () {
         if (this.checked) {
             $('#chocolatequantity').show();
+            $('#chocolate-img').show();
         } else {
             $('#chocolatequantity').hide();
+            $('#chocolate-img').hide();
         }
     })
 
     $('#strawberry').click(function () {
         if (this.checked) {
             $('#strawberryquantity').show();
+            $('#strawberry-img').show();
         } else {
             $('#strawberryquantity').hide();
+            $('#strawberry-img').hide();
         }
     })
 
     $('#banana').click(function () {
         if (this.checked) {
             $('#bananaquantity').show();
+            $('#banana-img').show();
         } else {
             $('#bananaquantity').hide();
+            $('#banana-img').hide();
         }
     })
 
     $('#coffee').click(function () {
         if (this.checked) {
             $('#coffeequantity').show();
+            $('#coffee-img').show();
         } else {
-            $('#quantity').hide();
+            $('#coffeequantity').hide();
+            $('#coffee-img').hide();
         }
     })
 })
